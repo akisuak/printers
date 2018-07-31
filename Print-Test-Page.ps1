2bc43dd389af7b688f38fe23530bebc8294f1059
akisuak <41571489+akisuak@users.noreply.github.com>
2018-07-31 14:07:59 +0200

    Add files via upload

---
+++ Print-Test-Page.ps1
@@ -0,0 +1,18 @@
+﻿
+
+# Get the printer objects from WMI:
+$Printers = Get-CimInstance -ClassName Win32_Printer
+
+# Display the number of printers defined:
+# '{0} Printers defined on this system' -f $Printers.Count
+
+#Get the "\\PS5\Follow-me" printer:
+$Printer = $Printers | Where-Object Name -eq "\\PS5\Follow-me"
+
+# Display the printer's details:
+# $Printer | Format-Table -AutoSize
+
+#Print a test page:
+Invoke-CimMethod -InputObject $Printer -MethodName PrintTestPage
+
+
---
+++ add-printer-ports.ps1
@@ -0,0 +1,39 @@
+﻿
+#----------------------------------------
+# Server: KUITPRT01PW.unicph.domain
+#----------------------------------------
+
+# Due to timetrouble, Printers are named with the present names,
+# but not the ports! 
+
+<# TEOL
+Add-PrinterPort -Name "5_000_001" -PrinterHostAddress "10.76.34.11"
+Add-PrinterPort -Name "5_000_002" -PrinterHostAddress "10.76.34.12"
+Add-PrinterPort -Name "5_000_003" -PrinterHostAddress "10.76.34.13"
+Add-PrinterPort -Name "5_000_004" -PrinterHostAddress "10.76.34.14"
+Add-PrinterPort -Name "5_000_005" -PrinterHostAddress "10.76.34.17"
+Add-PrinterPort -Name "5_000_006" -PrinterHostAddress "10.76.34.21"
+Add-PrinterPort -Name "5_000_007" -PrinterHostAddress "10.76.34.22"
+Add-PrinterPort -Name "5_000_008" -PrinterHostAddress "10.76.34.23"
+Add-PrinterPort -Name "5_000_009" -PrinterHostAddress "10.76.34.24"
+Add-PrinterPort -Name "5_000_010" -PrinterHostAddress "10.76.34.25"
+Add-PrinterPort -Name "5_000_011" -PrinterHostAddress "10.76.34.26"
+Add-PrinterPort -Name "5_000_012" -PrinterHostAddress "10.76.34.41"
+Add-PrinterPort -Name "5_000_013" -PrinterHostAddress "10.76.34.42"
+Add-PrinterPort -Name "5_000_014" -PrinterHostAddress "10.76.34.43"
+Add-PrinterPort -Name "5_000_015" -PrinterHostAddress "10.76.34.44"
+Add-PrinterPort -Name "5_000_016" -PrinterHostAddress "10.76.34.47"
+Add-PrinterPort -Name "5_000_017" -PrinterHostAddress "10.76.34.48"
+Add-PrinterPort -Name "5_000_018" -PrinterHostAddress "10.76.34.46"
+#>
+
+# HR-SC
+# Add-PrinterPort -Name "5_000_019" -PrinterHostAddress "10.41.44.56"
+
+#----------------------------------------
+# Server: KUITPRT02PW.unicph.domain
+#----------------------------------------
+
+# HR-SC
+# Add-PrinterPort -Name "5_000_020" -PrinterHostAddress "10.41.44.55"
+# Add-PrinterPort -Name "5_000_021" -PrinterHostAddress "10.41.44.54"
