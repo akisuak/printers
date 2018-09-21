690c2b6a76b18ea709c236a3a489ddd90c443756
akisuak <41571489+akisuak@users.noreply.github.com>
2018-09-21 11:01:02 +0200

    Add files via upload

---
+++ prt05-add-printer-ports.ps1
@@ -0,0 +1,14 @@
+﻿
+# Server: KUITPRT05PW.unicph.domain
+# Ports 
+
+# Add-PrinterPort -Name "5_000_" -PrinterHostAddress "10..." # printernavn 
+
+<#
+Add-PrinterPort -Name "5_000_067" -PrinterHostAddress "10.76.20.21" # TEST-KM KONICA MUS 5-1-05
+Add-PrinterPort -Name "5_000_068" -PrinterHostAddress "10.76.20.148" # TEST-R RICOH MUS 5-1-05
+#>
+
+# Add-PrinterPort -Name "5_000_069" -PrinterHostAddress "10.76.20.149" # TEST-R307 RICOH MUS 5-1-05
+
+ 
---
+++ prt05-add-printer.ps1
@@ -0,0 +1,20 @@
+﻿
+# Server: KUITPRT05PW.unicph.domain
+
+ 
+# Add-Printer -Name "navn" -DriverName "driver" -PortName "5_000_" -Location "location" # -Published -Shared
+
+<#
+Add-Printer -Name "TEST-KM" -DriverName "KONICA MINOLTA Universal PCL" -PortName "5_000_067" -Location "MUS 5-1-05"  -Published -Shared
+Add-Printer -Name "TEST-R" -DriverName "PCL6 Driver for Universal Print" -PortName "5_000_068" -Location "MUS 5-1-05"  -Published -Shared
+#>
+
+# Add-Printer -Name "TEST-R307" -DriverName "PCL6 Driver for Universal Print" -PortName "5_000_069" -Location "MUS 5-1-05"  -Published -Shared
+
+
+
+
+
+
+
+
